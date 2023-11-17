4f8512a1087df19df0a8fa31f7b84858e3be7fae
santiagomartinez07 <141145926+santiagomartinez07@users.noreply.github.com>
2023-11-17 12:40:23 -0500

    Update INTENTO 5.asm

--- INTENTO 5.asm
+++ INTENTO 5.asm
@@ -1,64 +1,195 @@
 ; Template para Algoritmo de booth
-
-; Observe que los comentarios están especificados usando ";"
-; por lo tanto, todo lo que encuentre despues de este simbolo así será tratado.
-
-; Hasta este punto del curso se asume llena la información a operar en memoria
-; Acá se describe como cargar los datos para la ejecución y como usarlos
-
-; Cada variable que se desee usar debe definirse usando un nombre, una serie de caracteres (Palabras) 
-; Para identificar ques es una variable se debe usar ":" y posteriormente agregar el valor inicial
-; el valor inicial puede agregarse en binario o hexadecimal, recuerde agregar el prefijo correspondiente
-; "0b" para binario o "0x para hexadecimal".
-; Aunque parece una asignación directa, recuerde que debe usar el DTPR para acceder a esta información
-; pues al hacer referencia al nombre de la variable, se accederá a la posición de memoria de la misma,
-; no a su contenido.
-
 ; Para el algoritmo de booth son necesarias las siguientes variables
-; aunque el nombre no es importante, es recomendable mantenerlo.
-; Es importante mantener el orden de estas variables, pues la visualización de la interfaz web
-; se basa en la dirección de memoria, no en el nombre de la variable.
 
-variableA: 0b0
+variableA: 0b0 
 Q: 0b10000001 ; Multiplicador
 Q_1: 0b0
 M: 0b11111101; Multiplicando
 count: 0x8
 
-; Es importante destacar una diferencia en este punto, un indice para realizar saltos en el código
-; se define usando el simbolo ":" al igual que la definición de una variable
-; La diferencia radica en la asignación del valor, es decir, si no se detecta valor, 
-; se establece esa pocisión de código como indice
+; Funciones
+FUNCTION TAM
+  ; Devuelve el tamaño del operando multiplicando
 
-inicio:
-; Es importante resaltar una diferencia en esta implementación,
-; por simplicidad en el código cuando se usa una instrucción que contiene "CTE"
-; esto se reemplaza bien sea por el valor (binario o hexadecimal) que se desea usar
-; o por el nombre de la variable / indice al cual se hace referencía
-; es decir: No se escribiría la siguiente instrucción de cargar dirección 
-; de memoria de Q en Acc como:
-; mov ACC, CTE
-; Q
-; en esta implementación se debe escribir así:
-; mov ACC, Q
+  ; Regresa el valor de TAM
+  RET CTE
+ENDFUNCTION
 
-; Evalúa sí Acc es cero
-jz load_a
-ind_1:
-; Se carga la variable Q a registro A
-mov ACC, Q
-mov A, ACC
-call fn_1
+FUNCTION SUMA
+  ; Suma los valores de Q(LSB) y A(MSB)
 
-hlt ; Se detiene la ejecucion
-load_a:
-; Se carga Variable A a Acc
-mov ACC, variableA
-mov A, ACC
-jz ind_1
-hlt
+  ; Carga el valor de Q(LSB) a ACC
+  MOV ACC, CTE
+  Q(LSB)
+  MOV DPTR, ACC
+  MOV ACC, [DPTR]
+  MOV A, ACC
 
-fn_1:
-; Se carga Variable count a Acc
-mov ACC, count
-ret ;retorna al punto donde se ejecutó CALL CTE
+  ; Carga el valor de A(MSB) a ACC
+  MOV ACC, CTE
+  A(MSB)
+  MOV DPTR, ACC
+  MOV ACC, [DPTR]
+  MOV A, ACC
+
+  ; Suma los valores de Q(LSB) y A(MSB)
+  ADD ACC, A
+
+  ; Guarda el resultado en Q(LSB)
+  MOV ACC, CTE
+  Q(LSB)
+  MOV DPTR, ACC
+  MOV [DPTR], ACC
+
+  ; Regresa al ciclo principal
+  RET CTE
+ENDFUNCTION
+
+FUNCTION RESTA
+  ; Resta los valores de Q(LSB) y A(MSB)
+
+  ; Carga el valor de Q(LSB) a ACC
+  MOV ACC, CTE
+  Q(LSB)
+  MOV DPTR, ACC
+  MOV ACC, [DPTR]
+  MOV A, ACC
+
+  ; Carga el valor de A(MSB) a ACC
+  MOV ACC, CTE
+  A(MSB)
+  MOV DPTR, ACC
+  MOV ACC, [DPTR]
+  MOV A, ACC
+
+  ; Resta los valores de Q(LSB) y A(MSB)
+  SUB ACC, A
+
+  ; Guarda el resultado en Q(LSB)
+  MOV ACC, CTE
+  Q(LSB)
+  MOV DPTR, ACC
+  MOV [DPTR], ACC
+
+  ; Regresa al ciclo principal
+  RET CTE
+ENDFUNCTION
+
+; Inicio del programa
+
+Init_Loop:
+  ; Inicializa el contador y el valor de la variable Q(LSB)
+  MOV ACC, CTE
+  TAM
+  MOV DPTR, ACC
+  MOV ACC, [DPTR]
+  INV ACC
+  MOV A, ACC
+
+  MOV ACC, CTE
+  0x01
+  ADD ACC, A
+  MOV A, ACC
+
+  MOV ACC, CTE
+  ITERATOR
+  MOV DPTR, ACC
+  MOV ACC, [DPTR]
+  ADD ACC, A
+  JZ CTE
+
+  ; Entra al ciclo principal
+  CALL CICLO_BOOTH
+
+Fin:
+  ; Termina el programa
+  HLT
+
+; Ciclo principal
+
+CICLO_BOOTH:
+  ; Carga el valor de Q(LSB) a ACC
+  MOV ACC, CTE
+  Q(LSB)
+  MOV DPTR, ACC
+  MOV ACC, [DPTR]
+  MOV A, ACC
+
+  ; Carga el valor de G a ACC
+  MOV ACC, CTE
+  G
+  MOV DPTR, ACC
+  MOV ACC, [DPTR]
+  MOV [DPTR], ACC
+
+  ; Carga el valor de Q-1 a ACC
+  MOV ACC, CTE
+  Q-1
+  MOV DPTR, ACC
+  MOV ACC, [DPTR]
+  INV ACC
+  MOV A, ACC
+
+  ; Suma 1 a ACC
+  MOV ACC, CTE
+  0x01
+  ADD ACC, A
+  MOV A, ACC
+
+  ; Carga el valor de G a ACC
+  MOV ACC, CTE
+  G
+  MOV DPTR, ACC
+  MOV ACC, [DPTR]
+  ADD ACC, A
+
+  ; Si ACC es 0, termina el ciclo
+  JZ CTE
+; Sub-ciclo de desplazamiento
+
+DESPLAZAMIENTO:
+  ; Carga el valor de Q(LSB) a ACC
+  MOV ACC, CTE
+  Q(LSB)
+  MOV DPTR, ACC
+  MOV ACC, [DPTR]
+  MOV A, ACC
+
+  ; Guarda el valor de Q-1 en Q(LSB)
+  MOV ACC, CTE
+  Q-1
+  MOV DPTR, ACC
+  MOV [DPTR], ACC
+
+  ; Carga el valor de Q(MSB) a ACC
+  MOV ACC, CTE
+  Q(MSB)
+  MOV DPTR, ACC
+  MOV ACC, [DPTR]
+  MOV A, ACC
+
+  ; Shiftea a la izquierda el valor de Q(MSB)
+  MOV ACC, A
+  SHR ACC, 1
+  MOV [DPTR], ACC
+
+  ; Carga el valor de A(MSB) a ACC
+  MOV ACC, CTE
+  A(MSB)
+  MOV DPTR, ACC
+  MOV ACC, [DPTR]
+  MOV A, ACC
+
+  ; Shiftea a la izquierda el valor de A(MSB)
+  MOV ACC, A
+  SHR ACC, 1
+  MOV [DPTR], ACC
+
+  ; Regresa al ciclo principal
+  CALL CICLO_BOOTH
+
+; Fin del programa
+
+Fin:
+  ; Termina el programa
+  HLT
